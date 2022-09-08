0a157a48a0b4b9592a2c74e47dbeb57490280fb4
Vanessa Otto <boehner.vanessa@gmail.com>
2022-09-08 08:15:16 +0200

    removes shadow

--- src/components/SideNavigation.vue
+++ src/components/SideNavigation.vue
@@ -3,7 +3,7 @@
 <template>
   <nav aria-label="alternative nav">
     <div
-      class="bg-white dark:bg-gray-800 shadow-xl h-20 fixed bottom-0 mt-12 md:relative md:h-screen z-10 w-full md:w-48 content-center"
+      class="bg-white dark:bg-gray-800 h-20 fixed bottom-0 mt-12 md:relative md:h-screen z-10 w-full md:w-48 content-center"
     >
       <div
         class="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between"
