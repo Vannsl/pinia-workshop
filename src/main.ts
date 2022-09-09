b8f72543ccae4de0ddf706c987cfbbf9f4191d2d
Vanessa Otto <boehner.vanessa@gmail.com>
2022-09-09 13:19:11 +0200

    A: installs and registers pinia, creates useStatisticStore.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import router from "./router/router";
+import { createPinia } from "pinia";
 import "./style.css";
 import "./index.css";
 import App from "./App.vue";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
---
+++ src/stores/useStatisticStore.ts
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia";
+
+export const useStatisticStore = defineStore("Statistic", {
+  state: () => {
+    return {};
+  },
+});
