6ff6b12b8e09c0eaaa4c9b3b0f6662f57e0416cd
Vanessa Otto <boehner.vanessa@gmail.com>
2022-09-09 13:23:49 +0200

    B: moves state into statistic store, adding HMR function

--- src/stores/useStatisticStore.ts
+++ src/stores/useStatisticStore.ts
@@ -1,7 +1,14 @@
-import { defineStore } from "pinia";
+import { acceptHMRUpdate, defineStore } from "pinia";
+import stats from "@/data/stats.json";
 
 export const useStatisticStore = defineStore("Statistic", {
   state: () => {
-    return {};
+    return {
+      stats,
+    };
   },
 });
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useStatisticStore, import.meta.hot));
+}
