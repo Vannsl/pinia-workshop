1669272b237b7cb4a906cff8e3468ea0d4b0ee8c
Vanessa Otto <boehner.vanessa@gmail.com>
2022-09-20 12:03:45 +0200

    adds dummy functions

--- src/components/TodoListItemActive.vue
+++ src/components/TodoListItemActive.vue
@@ -3,11 +3,23 @@ import { useI18n } from "vue-i18n";
 import type { TodoItem } from "@/types/TodoItem";
 import IconBin from "./icons/IconBin.vue";
 
-defineProps<{
+/* eslint-disable */
+const props = defineProps<{
   todoItem: TodoItem;
 }>();
 
+const emit = defineEmits(["on-change", "on-delete"]);
+
 const { t } = useI18n();
+
+function handleChange(event: Event) {
+  // TODO
+}
+
+function handleDelete() {
+  // TODO
+}
+/* eslint-enable */
 </script>
 
 <template>
@@ -21,7 +33,7 @@ const { t } = useI18n();
         :id="`todo_${todoItem.id}`"
         :value="todoItem.value"
         :checked="todoItem.isCompleted"
-        @change="$emit('on-change', todoItem.id)"
+        @change="handleChange"
       />
       <span :class="{ 'line-through': todoItem.isCompleted }">
         {{ todoItem.value }}
@@ -30,7 +42,7 @@ const { t } = useI18n();
     <button
       class="group-hover:visible invisible hover:text-blue-500 dark:hover:text-blue-200"
       type="button"
-      @click.stop="$emit('on-delete', todoItem.id)"
+      @click.stop="handleDelete"
     >
       <IconBin :aria-label="t('delete')" class="w-5 h-5" />
     </button>
