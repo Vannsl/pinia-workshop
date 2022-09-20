28df583d62c5051c3621d28d55f3cd9f6242cab3
Vanessa Otto <boehner.vanessa@gmail.com>
2022-09-20 13:08:24 +0200

    adds transitiongroup

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -28,7 +28,7 @@ const hasArchivedTodoItems = computed(() => archivedTodoItems.value.length > 0);
       <h2 class="font-semibold text-xl mb-2">
         {{ t("your_items") }}
       </h2>
-      <ul>
+      <TransitionGroup name="list" tag="ul">
         <li v-for="todoItem in activeTodoItems" :key="todoItem.id">
           <TodoListItemActive
             :todo-item="todoItem"
@@ -36,20 +36,40 @@ const hasArchivedTodoItems = computed(() => archivedTodoItems.value.length > 0);
             @on-delete="() => {}"
           />
         </li>
-      </ul>
+      </TransitionGroup>
     </div>
     <div v-if="hasArchivedTodoItems">
       <h2 class="font-semibold text-xl mb-2">
         {{ t("deleted_items") }}
       </h2>
-      <ul>
+      <TransitionGroup name="list" tag="ul">
         <li v-for="todoItem in archivedTodoItems" :key="todoItem.id">
           <TodoListItemArchived :todo-item="todoItem" @on-click="() => {}" />
         </li>
-      </ul>
+      </TransitionGroup>
     </div>
   </template>
   <p v-else>
     {{ t("no_todos_yet") }}
   </p>
 </template>
+
+<style scoped>
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
+}
+</style>
