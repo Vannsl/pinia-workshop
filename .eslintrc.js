8c54e0fe0b4dc201ae064e3b006fb2ef823afbd0
Vanessa Otto <boehner.vanessa@gmail.com>
2022-09-20 11:57:44 +0200

    adds eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,4 +10,8 @@ module.exports = {
   env: {
     "vue/setup-compiler-macros": true,
   },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
 };
