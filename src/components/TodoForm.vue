4f3b8b0cb659f810971940c5ad7a182b78a716ed
Vanessa Otto <boehner.vanessa@gmail.com>
2022-10-02 10:50:59 +0200

    adds createItem function

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useI18n } from "vue-i18n";
+import { nanoid } from "nanoid";
+import type { TodoItem } from "@/types/TodoItem";
 import BaseButton from "./base/BaseButton.vue";
 
 const { t } = useI18n();
 
 const newItem = ref("");
 
+function createItem(value: TodoItem["value"]) {
+  return {
+    id: nanoid(),
+    value,
+    isCompleted: false,
+    isArchived: false,
+  };
+}
+
 function handleSubmit() {
   const trimmedValue = newItem.value.trim();
   if (!trimmedValue) return;
 
   newItem.value = "";
+  const item = createItem(trimmedValue);
 
   // TODO
+  console.log(item);
 }
 </script>
 
