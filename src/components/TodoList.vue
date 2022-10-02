c87e30adadb820cc932fe1961d3403633542dcce
Vanessa Otto <boehner.vanessa@gmail.com>
2022-10-02 10:16:16 +0200

    D: replaces computed properties with store getters

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 import { useTodoListStore } from "@/stores/useTodoListStore";
 import TodoListItemActive from "./TodoListItemActive.vue";
@@ -8,25 +7,19 @@ import TodoListItemArchived from "./TodoListItemArchived.vue";
 const { t } = useI18n();
 
 const todoListStore = useTodoListStore();
-
-const activeTodoItems = computed(() =>
-  todoListStore.todoItems
-    .filter(({ isArchived }) => !isArchived)
-    .sort((item) => (item.isCompleted ? 1 : -1))
-);
-const archivedTodoItems = computed(() =>
-  todoListStore.todoItems.filter(({ isArchived }) => isArchived)
-);
 </script>
 
 <template>
-  <template v-if="true">
-    <div v-if="true">
+  <template v-if="todoListStore.hasTodoItems">
+    <div v-if="todoListStore.hasActiveTodoItems">
       <h2 class="font-semibold text-xl mb-2">
         {{ t("your_items") }}
       </h2>
       <TransitionGroup name="list" tag="ul">
-        <li v-for="todoItem in activeTodoItems" :key="todoItem.id">
+        <li
+          v-for="todoItem in todoListStore.activeTodoItems"
+          :key="todoItem.id"
+        >
           <TodoListItemActive
             :todo-item="todoItem"
             @on-input-change="() => {}"
@@ -35,12 +28,15 @@ const archivedTodoItems = computed(() =>
         </li>
       </TransitionGroup>
     </div>
-    <div v-if="true">
+    <div v-if="todoListStore.hasArchivedTodoItems">
       <h2 class="font-semibold text-xl mb-2">
         {{ t("archived_items") }}
       </h2>
       <TransitionGroup name="list" tag="ul">
-        <li v-for="todoItem in archivedTodoItems" :key="todoItem.id">
+        <li
+          v-for="todoItem in todoListStore.archivedTodoItems"
+          :key="todoItem.id"
+        >
           <TodoListItemArchived :todo-item="todoItem" @on-click="() => {}" />
         </li>
       </TransitionGroup>
