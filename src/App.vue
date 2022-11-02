dd529f1a77aab2af53ecba15cf4d8a5c7f99a415
Vanessa Otto <boehner.vanessa@gmail.com>
2022-11-02 17:42:08 +0100

    vanessa has a question

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
 import SideNavigation from "./components/SideNavigation.vue";
 import TheHeader from "./components/TheHeader.vue";
+import { useAuthenticationStore } from "./stores/useAuthenticationStore";
+
+const userStore = useAuthenticationStore();
+const isLoaded = ref(false);
+
+function sleep(milliseconds = 500) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
+onMounted(async () => {
+  await sleep();
+  userStore.user = { id: 1, fullName: "Vanessa Otto" };
+  isLoaded.value = true;
+});
 </script>
 
 <template>
@@ -10,7 +25,7 @@ import TheHeader from "./components/TheHeader.vue";
       <SideNavigation />
       <section class="w-full">
         <div class="flex-1 bg-gray-100 dark:bg-gray-900 mt-12 md:mt-2 h-full">
-          <router-view />
+          <router-view v-if="isLoaded" />
         </div>
       </section>
     </div>
